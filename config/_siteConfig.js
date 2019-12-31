f5fc7014cb4d5b0ff0dc3aa73bd2a92b9fd542a5
Brendan Lensink <blensink192@gmail.com>
2019-12-31 15:32:01 -0800

    Update _siteConfig.js
    set title and tagline

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -9,12 +9,12 @@ import theCMS from '../cms/netlify/cms'
  * General Site configurations
  */
 export default {
-  siteName: 'Awake',
-  tagline: 'A Nuxt.js Theme for Netlify CMS',
+  siteName: 'Brendan Lensink',
+  tagline: 'If it\'s worth doing, it\'s worth overdoing',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
-    on: true,
+    on: false,
     id: process.env.GOOGLE_ANALYTICS_ID
   },
   mainMenu: [
