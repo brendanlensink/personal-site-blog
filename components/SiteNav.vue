3853ee82d314261f6f7efea12f1d63702ff11004
Brendan Lensink <blensink192@gmail.com>
2019-12-31 15:56:15 -0800

    Update SiteNav.vue

--- components/SiteNav.vue
+++ components/SiteNav.vue
@@ -4,46 +4,6 @@
     role="navigation"
     aria-label="main navigation"
   >
-    <div class="navbar-brand">
-      <nuxt-link class="navbar-item" to="/">
-        <site-logo v-if="$siteConfig.logo === 'logo-component'" />
-        <img
-          v-else
-          :src="$siteConfig.logo"
-          :alt="$siteConfig.siteName"
-          class="logo"
-        />
-      </nuxt-link>
-      <hamburger-button @click="active = !active" />
-    </div>
-
-    <div
-      :class="{
-        'navbar-menu': true,
-        'is-active': active
-      }"
-    >
-      <ul class="navbar-end">
-        <li
-          v-for="item in $siteConfig.mainMenu"
-          :key="item.link"
-          class="navbar-item"
-          @click="active = false"
-        >
-          <component
-            :is="item.link.startsWith('http') ? 'a' : 'nuxt-link'"
-            :href="item.link"
-            :to="item.link"
-            :target="item.target ? item.target : '_self'"
-          >
-            {{ item.name }}
-          </component>
-        </li>
-        <li class="navbar-item site-search-wrapper">
-          <site-search />
-        </li>
-      </ul>
-    </div>
   </nav>
 </template>
 <script>
