7939bf0b32116565820de4b75e2a6fe8641dbb49
Brendan Lensink <blensink192@gmail.com>
2019-12-31 16:05:26 -0800

    Update NewsLetterFormModal.vue

--- components/NewsLetterFormModal.vue
+++ components/NewsLetterFormModal.vue
@@ -1,12 +1,5 @@
 <template>
-  <modal-overlay
-    id="newsletter-modal"
-    :title="title"
-    trigger-text="Subscribe To Newsletter"
-    class="newsletter-modal"
-  >
-    <news-letter-form />
-  </modal-overlay>
+
 </template>
 <script>
 import ModalOverlay from '~/components/ModalOverlay'
